c0a84b35de5ccb569913396d492385ea2f951e4c
paul-harness <56152762+paul-harness@users.noreply.github.com>
2022-05-05 13:37:08 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,7 @@
-echo "Application:" ${PLUGIN_APPLICATION}
-echo "Image Name:" ${PLUGIN_IMAGENAME}
+echo "Username:" ${PLUGIN_USERNAME}
+echo "Password:" ${PLUGIN_PASSWORD}
+echo "Liquidbase Parameters:" ${PLUGIN_LIQUIBASEPARAMS}
+echo "Liquidbase Context:" ${PLUGIN_LIQUIDBASECONTEXT}
+echo "Schema:" ${PLUGIN_SCHEMA}
 echo "Repo:" ${PLUGIN_REPO}
-echo "Branch:" ${PLUGIN_BRANCH}
-echo "Commit SHA:" ${PLUGIN_COMMITSHA}
-echo "Variables Passed to CARA Nolio API SUCCESSFULLY!"
+echo "Variables Passed to Liquidbase API SUCCESSFULLY!"
