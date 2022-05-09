e6b2ee56894529fb7e42cfcf9a46f25cc3230773
paul-harness <56152762+paul-harness@users.noreply.github.com>
2022-05-09 07:55:41 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 echo "URL to search:" ${PLUGIN_URL}
 echo "Search Term:" ${PLUGIN_SEARCHTERM}
 
-git clone https://github.com/paul-harness/simple-selenium-project.git
+git clone -b advanced --single-branch  https://github.com/paul-harness/simple-selenium-project.git
 cd simple-selenium-project
 python3 paul-selenium.py > output.txt
 cat output.txt
