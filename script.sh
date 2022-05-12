f71a3a81065136a47d43abd08f7a4fbe51a664cf
paul-harness <56152762+paul-harness@users.noreply.github.com>
2022-05-12 05:55:14 -0400

    Delete script.sh

--- script.sh
+++
@@ -1,7 +0,0 @@
-echo "URL to search:" ${PLUGIN_URL}
-echo "Search Term:" ${PLUGIN_SEARCHTERM}
-
-git clone https://github.com/paul-harness/simple-selenium-project.git
-cd simple-selenium-project
-python3 paul-selenium.py > output.txt
-cat output.txt
